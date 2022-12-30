946949d42fb442f8c6b128dbbde518b183187e9b
Android-Piepaint <73344795+Android-Piepaint@users.noreply.github.com>
2022-12-30 11:24:01 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,31 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+// support for .env, .env.development, and .env.production
+require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby + Vercel`,
-    siteUrl: `https://gatsby-template.vercel.app/`,
+    siteUrl: "https://gatsbydrupalhomepage.gatsbyjs.io/",
+    title: "Gatsby Drupal Homepage Starter",
+    author: `Gatsby`,
+    description: "A Gatsby Starter for building homepages with Drupal",
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: process.env.DRUPAL_BASE_URL,
+        basicAuth: {
+          username: process.env.DRUPAL_BASIC_AUTH_USERNAME,
+          password: process.env.DRUPAL_BASIC_AUTH_PASSWORD,
+        },
+        fastBuilds: true,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-vanilla-extract",
+  ],
+}
